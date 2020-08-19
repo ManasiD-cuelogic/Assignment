1f3df8d9f1063bb058811ba210c4d1f81c4dfe56
ManasiD-cuelgoic <manasi.deshmukh@cuelogic.com>
2020-08-19 08:53:24 +0530

    add new mwthod multiply

--- calculator.py
+++ calculator.py
@@ -1,10 +1,13 @@
 def add(a,b):
     sum=a+b
     print("Addition = ",sum)
-
+def multiply(a,b):
+    mult=a*b
+    print("Multiply = ",mult)
 if __name__ == __"main"__:
     print("Enter Fisrt Number")
     x=int(input))
     print("Enter Second Number")
     y=int(input())
     add(x,y)
+    multiply(x,y)
