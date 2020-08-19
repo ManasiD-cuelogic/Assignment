c558956c2a12b98f3d7f6004e74499f51b3c33a8
ManasiD-cuelgoic <manasi.deshmukh@cuelogic.com>
2020-08-19 11:04:08 +0530

    remove method multiply

--- calculator.py
+++ calculator.py
@@ -1,13 +1,9 @@
 def add(a,b):
     sum=a+b
     print("Addition = ",sum)
-def multiplication(a,b):
-    mult=a*b
-    print("Multiply = ",mult)
 if __name__ == __"main"__:
     print("Enter Fisrt Number")
     x=int(input))
     print("Enter Second Number")
     y=int(input())
     add(x,y)
-    multiplication(x,y)
