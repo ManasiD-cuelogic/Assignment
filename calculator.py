ad9284d4a6bed26abf55cee5de24b78643b51e14
ManasiD-cuelgoic <manasi.deshmukh@cuelogic.com>
2020-08-19 07:22:37 +0530

    add new feature with subtract method

--- calculator.py
+++ calculator.py
@@ -1,9 +1,13 @@
 def add(a,b):
     sum=a+b
     print("Addition = ",sum)
+def substract(a,b):
+    sub=a-b
+    print("Substraction = ",sub)
 if __name__ == __"main"__:
     print("Enter Fisrt Number")
     x=int(input))
     print("Enter Second Number")
     y=int(input())
     add(x,y)
+    sub(x,y)
