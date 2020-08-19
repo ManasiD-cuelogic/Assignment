b1d2e670f107c1850dba861d50fa29a4bcca0cfa
ManasiD-cuelgoic <manasi.deshmukh@cuelogic.com>
2020-08-19 07:18:26 +0530

    add new file with method add

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    sum=a+b
+    print("Addition = ",sum)
+if __name__ == __"main"__:
+    print("Enter Fisrt Number")
+    x=int(input))
+    print("Enter Second Number")
+    y=int(input())
+    add(x,y)
